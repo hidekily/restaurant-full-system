a040fa3ea3aa92599cb9b3a2a5363b7cbb45328d
Denilson Kuratomi <mhtaue@gmail.com>
2026-02-11 17:18:05 +0800

    npx tsc

--- shared/dist/auth/index.d.ts
+++ shared/dist/auth/index.d.ts
@@ -2,6 +2,15 @@ export declare const auth: import("better-auth").Auth<{
     database: (options: import("better-auth").BetterAuthOptions) => import("better-auth").DBAdapter<import("better-auth").BetterAuthOptions>;
     trustedOrigins: string[];
     baseURL: string;
+    advanced: {
+        crossSubDomainCookies: {
+            enabled: false;
+        };
+        defaultCookieAttributes: {
+            sameSite: "none";
+            secure: true;
+        };
+    };
     socialProviders: {
         google: {
             prompt: "select_account";
